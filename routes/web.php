a9c2fbb588d13b4c4a508cac0df68ba4d7ae48ea
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-05-19 00:05:11 +0200

    feat: added simple routes for side sites

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Home::class, 'getFiles']);
+
+Route::get('/contact', function(){
+    return view('contact');
+})->name('contact');
+
+
+Route::get('/login', function(){
+    return view('auth.login');
+})->name('login');
+
+
+Route::get('/register', function(){
+    return view('auth.register');
+})->name('register');
