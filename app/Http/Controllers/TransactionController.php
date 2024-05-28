9b04f4cbcf20291280d8ca69ebafbf6ff13d1813
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-05-28 17:44:35 +0200

    feat: transaction gate authorization

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Models\Offer;
 use App\Models\Transaction;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     public function getTransactions(User $user) {
+
+        Gate::authorize('view', $user);
         $transactions = Transaction::where('user_id', $user->id)
             ->get();
         //dd($transactions);
