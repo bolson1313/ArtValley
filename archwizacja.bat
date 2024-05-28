a18a319ef2c3c594d5448a0306cc0f909648e6df
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-05-28 19:19:33 +0200

    better archive

--- archwizacja.bat
+++ archwizacja.bat
@@ -1,7 +1,2 @@
-git init
-git config --local user.name "127754"
-git config --local user.email "127754@stud.ur.edu.pl"
-git add --all
-git commit -m "ArtValley"
 git archive --format=zip HEAD -o ../127754-AI1-projekt.zip
 pause
