98dbafa06dbd3442ecfe0a7ebfec67a1e4e1eb16
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-05-28 17:43:24 +0200

    ref: offer model

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Offer extends Model
@@ -26,8 +27,8 @@ class Offer extends Model
         return $this->belongsTo(ArtWork::class);
     }
 
-    public function transaction(): BelongsTo {
-        return $this->belongsTo(Offer::class);
+    public function transaction(): HasMany {
+        return $this->HasMany(Offer::class);
     }
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
