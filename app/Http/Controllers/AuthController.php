5cfc8a890776fca0f75b1dd542d5b9af823dbe50
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-05-28 17:38:40 +0200

    feat: middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
@@ -66,17 +68,26 @@ class AuthController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
      */
-    public function register(Request $request)
+    public function register(StoreUserRequest $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+        $request->session()->regenerateToken();
+        if ($request->hasFile('file_input')) {
+            $file = $request->file('file_input');
+            $imageName = $request->input('name') . '-' . $request->input('nickname') . '-' . time() . '.' . $file->extension();
+            $file->move(public_path('storage/img/avatars/'), $imageName);
+        } else {
+            $imageName = $request->input('name') . '-' . $request->input('nickname') . '-' . time() . '.png';
+            Storage::copy('public/blank-profile.png', 'public/img/avatars/' . $imageName);
+        }
+
 
         $user = User::create([
             'name' => $request->name,
+            'surname' => $request->surname,
+            'nickname' => $request->nickname,
+            'avatar' => $imageName,
             'email' => $request->email,
+            'remember_token' => $request->_token,
             'password' => Hash::make($request->password),
         ]);
 
